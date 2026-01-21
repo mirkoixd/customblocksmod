8606a9cba348955a9c6cbffb330bf73c9d02bdce
mirkoi <63493286+mirkoixd@users.noreply.github.com>
2026-01-21 23:00:41 +0300

    Create DataGenerators.java

---
+++ src/main/java/com/example/customblocksmod/datagen/DataGenerators.java
@@ -0,0 +1,21 @@
+package com.example.customblocksmod.datagen;
+
+import com.example.customblocksmod.CustomBlocksMod;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+import net.minecraftforge.data.DataGenerator;
+
+@Mod.EventBusSubscriber(modid = CustomBlocksMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator gen = event.getGenerator();
+        ExistingFileHelper helper = event.getExistingFileHelper();
+
+        gen.addProvider(new ModBlockStateProvider(gen, helper));
+        gen.addProvider(new ModItemModelProvider(gen, helper));
+    }
+}
