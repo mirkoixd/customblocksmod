6a5a60a0fde82a9c9b340215607714d110d100cf
mirkoi <63493286+mirkoixd@users.noreply.github.com>
2026-01-21 22:57:41 +0300

    Create CustomBlocksMod.java

---
+++ src/main/java/com/example/customblocksmod/CustomBlocksMod.java
@@ -0,0 +1,16 @@
+package com.example.customblocksmod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(CustomBlocksMod.MODID)
+public class CustomBlocksMod {
+    public static final String MODID = "customblocksmod";
+
+    public CustomBlocksMod() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModBlocks.BLOCKS.register(bus);
+        ModItems.ITEMS.register(bus);
+    }
+}
